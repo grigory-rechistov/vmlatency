5233f00ea653556c3c4a197aea7b82c856366e65
Grigory Rechistov <grigory.rechistov@phystech.edu>
2014-09-29 13:41:21 +0400

    Added frequency scaler disabling

--- collect-data.sh
+++ collect-data.sh
@@ -1,5 +1,18 @@
-FNM=exp-`date +%s`
+FNM=exp-`date +%s`.txt
+echo "Experiment name: $FNM"
+
+echo "Disabling frequency scaling"
+for CPUFREQ in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor;
+do
+    [ -f $CPUFREQ ] || continue;
+    echo -n performance > $CPUFREQ;
+done
+
 cat /proc/cpuinfo |head --lines 26 > $FNM
 lsb_release -a >> $FNM
+
+echo "Inserting/removing the kernel module"
 sudo insmod ./vmlaunch_simple.ko ; sudo rmmod vmlaunch_simple
 dmesg |tail --lines 31 >> $FNM
+
+echo "Done"
